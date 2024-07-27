166a1d87c3d960cd96c48c23fe495c03c144eb09
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-07-27 14:25:06 +0400

    Initial commit

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Abstraction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Animal donkey = new Donkey();
+
+            dog.AnimalSound();
+            donkey.AnimalSound();
+
+        }
+    }
+
+    public abstract class Animal
+    {
+        public abstract void AnimalSound();
+    }
+
+    public class Dog : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Dog ham-ham sound make!");
+        }
+    }
+
+    public class Donkey : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Donkey ia-ia sound make!");
+        }
+    }
+
+
+
+
+}
